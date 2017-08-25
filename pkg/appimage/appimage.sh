c8cf137a5f121e13e15141ca0ba01df54bfc03b7
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 13:31:38 +0900

    Update appimage.sh

--- pkg/appimage/appimage.sh
+++ pkg/appimage/appimage.sh
@@ -2,8 +2,12 @@
 
 PKGNAME='ocsstore'
 
-make
+make ocsmanager_build="appimage"
 make DESTDIR="${PKGNAME}.AppDir" prefix="/usr" install
+
+cp ${PKGNAME}.AppDir/usr/bin/${PKGNAME} ${PKGNAME}.AppDir/AppRun
+cp ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop ${PKGNAME}.AppDir/${PKGNAME}.desktop
+cp ${PKGNAME}.AppDir/usr/share/icons/hicolor/scalable/apps/${PKGNAME}.svg ${PKGNAME}.AppDir/${PKGNAME}.svg
 
 curl -L -o appimagetool.AppImage "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod 755 appimagetool.AppImage
