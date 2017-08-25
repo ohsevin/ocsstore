c0bb1bc853bce0bae5327623450c50bf79012451
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 15:04:44 +0900

    Fix path

--- launcher/ocsstore.sh
+++ launcher/ocsstore.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ -f './ocsstore-linux-x64/ocsstore' ]; then
-    ./ocsstore-linux-x64/ocsstore
-elif [ -f './usr/local/lib/ocsstore-linux-x64/ocsstore' ]; then
-    ./usr/local/lib/ocsstore-linux-x64/ocsstore
-elif [ -f './usr/lib/ocsstore-linux-x64/ocsstore' ]; then
-    ./usr/lib/ocsstore-linux-x64/ocsstore
+PWD="$(cd "$(dirname "${0}")/../" && pwd)"
+
+if [ -f "${PWD}/ocsstore-linux-x64/ocsstore" ]; then
+    ${PWD}/ocsstore-linux-x64/ocsstore
+elif [ -f "${PWD}/usr/local/lib/ocsstore-linux-x64/ocsstore" ]; then
+    ${PWD}/usr/local/lib/ocsstore-linux-x64/ocsstore
+elif [ -f "${PWD}/usr/lib/ocsstore-linux-x64/ocsstore" ]; then
+    ${PWD}/usr/lib/ocsstore-linux-x64/ocsstore
 elif [ -f '/usr/local/lib/ocsstore-linux-x64/ocsstore' ]; then
     /usr/local/lib/ocsstore-linux-x64/ocsstore
 elif [ -f '/usr/lib/ocsstore-linux-x64/ocsstore' ]; then
