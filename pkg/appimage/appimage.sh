b1063f8c09ea15d25b2160379a874c00dbc8edfe
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 21:25:52 +0900

    Update appimage.sh

--- pkg/appimage/appimage.sh
+++ pkg/appimage/appimage.sh
@@ -9,8 +9,10 @@ cp ${PKGNAME}.AppDir/usr/bin/${PKGNAME} ${PKGNAME}.AppDir/AppRun
 cp ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop ${PKGNAME}.AppDir/${PKGNAME}.desktop
 cp ${PKGNAME}.AppDir/usr/share/icons/hicolor/scalable/apps/${PKGNAME}.svg ${PKGNAME}.AppDir/${PKGNAME}.svg
 
-curl -L -o appimagetool.AppImage "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-chmod 755 appimagetool.AppImage
-./appimagetool.AppImage --appimage-extract
+curl -L -o appimagetool "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod 755 appimagetool
+./appimagetool --appimage-extract
 
 ./squashfs-root/AppRun ${PKGNAME}.AppDir
+
+mv *.AppImage ${PKGNAME}-x86_64.AppImage
