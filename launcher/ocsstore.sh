0a26a360a74e7e1ec622b8d9b763a6a1575234f1
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 13:30:58 +0900

    Update launcher ocsstore.sh

--- launcher/ocsstore.sh
+++ launcher/ocsstore.sh
@@ -2,6 +2,10 @@
 
 if [ -f './ocsstore-linux-x64/ocsstore' ]; then
     ./ocsstore-linux-x64/ocsstore
+elif [ -f './usr/local/lib/ocsstore-linux-x64/ocsstore' ]; then
+    ./usr/local/lib/ocsstore-linux-x64/ocsstore
+elif [ -f './usr/lib/ocsstore-linux-x64/ocsstore' ]; then
+    ./usr/lib/ocsstore-linux-x64/ocsstore
 elif [ -f '/usr/local/lib/ocsstore-linux-x64/ocsstore' ]; then
     /usr/local/lib/ocsstore-linux-x64/ocsstore
 elif [ -f '/usr/lib/ocsstore-linux-x64/ocsstore' ]; then
